089502cbc9fb00edce10b808b85c7d1943f40994
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-04-29 03:24:07 +0530

    coin complete

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -3,25 +3,35 @@ import axios from 'axios'
 import { server } from '../index'
 import { Container, HStack, Heading, VStack, Image, Text } from '@chakra-ui/react'
 import Loader from './Loader'
+import Error from './Error'
 
 const Exchanges = () => {
     
     const [exchanges, setExchanges] = useState([])
-
+    const [error, setError] = useState(false)
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchExchanges = async () => {
-            const { data } = await axios.get(`${server}/exchanges`);
-            setExchanges(data);
-            console.log(data);
-            setLoading(false);
+            try {
+                const { data } = await axios.get(`${server}/exchanges`);
+                setExchanges(data);
+                console.log(data);
+                setLoading(false);
+            } catch (error) {
+                setError(true);
+                setLoading(false);
+                console.log(error);
+            }
 
     }
     fetchExchanges();
     }
     , [])
 
+    if(error) return <div><Error /></div>
+    
+    else{
     return (
         <Container maxW={"container.xl"} centerContent>
             { loading ? ( 
@@ -44,10 +54,17 @@ const Exchanges = () => {
         </Container>
     );  
 }
+}
 
 const ExchangeCard = ({name, img, rank, url}) => (
-    <a href={url} target={"blank"}>
-    <VStack>
+    <a href={url} target={"blank"}>     {/* target={"blank"} opens the link in same tab hence no wastage of tabs */}
+    <VStack w={"52"} shadow={"lg"} p={"8"} borderRadius={"lg"} transition={"all 0.3s"} m={"4"}
+    css={{
+        "&:hover": {
+            transform: "scale(1.05)",
+            boxShadow: "xl",
+        },
+    }}>
         <Image
             src={img}
             alt={"Exchanges"}
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,17 @@
+import { VStack, Box, Spinner } from '@chakra-ui/react'
 import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div>
+      <VStack h={"90vh"} justifyContent={"center"}>
+      <Box transform={"scale(3)"}>
+        <Spinner size={"xl"} />
+      </Box>
+
+      
+      </VStack>
+    </div>
   )
 }
 
