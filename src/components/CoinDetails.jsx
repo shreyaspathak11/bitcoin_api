842cccbc4edd2bb5341a392e435d3797053e0887
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-04-29 03:37:01 +0530

    rrr

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import Loader from './Loader'
+import { Box, Container } from '@chakra-ui/react'
+
+
 
 const coinDetails = () => {
+  const [coin, setCoin] = useState([])
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(true)
+    const [currency, setCurrency] = useState("inr")
   return (
-    <div>coinDetails</div>
+    <Container maxW="container.xl" >
+        {loading ? (<Loader />) : (
+          <>
+            <Box width={"full"} borderWidth={1}>
+              asdsad
+            </Box>
+          </>
+        )}
+      </Container>
+
   )
 }
 
