816b300e9c5d4c491e320e5af3233174e8a31c8d
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-05-01 02:18:32 +0530

    working on home page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme} >
       <App />
     </ChakraProvider>
   </React.StrictMode>
