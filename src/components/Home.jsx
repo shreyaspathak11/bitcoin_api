82da6879022cd3eb5f20e7ceffd05617357bc574
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-05-01 23:17:12 +0530

    working on Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,51 +1,46 @@
-import React from 'react'
-import {
-  Box,
-  Container,
-  Wrap,
-  WrapItem,
-  Text,
-  Image,
-} from "@chakra-ui/react";
 
+import React from 'react'
+import { Box, Flex, Heading, Text, Button, Image } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import Hero from '../assets/increase.png'
 
 const Home = () => {
+
+
   return (
-    
-      <Box>
-      <Box bg="gray.50">
-          <Container maxW="container.2xl" centerContent py={[20]}>
-          <Text color="gray.600" fontSize="lg">
-            Used by teams worldwide
-          </Text>
-
-          <Wrap
-            spacing={[10, 20]}
-            mt={8}
-            align="center"
-            justify="center"
-            w="full"
-          >
-            <WrapItem>
-              <Image src="microsoft-logo.svg" alt="Microsoft logo" />
-            </WrapItem>
-
-            <WrapItem>
-              <Image src="adobe-logo.svg" alt="Adobe logo" />
-            </WrapItem>
-
-            <WrapItem>
-              <Image src="microsoft-logo.svg" alt="Microsoft logo" />
-            </WrapItem>
-
-            <WrapItem>
-              <Image src="adobe-logo.svg" alt="Adobe logo" />
-            </WrapItem>
-          </Wrap>
-        </Container>
-      </Box>
+    <Flex p={4} align="center" justify="center">
+    <Box w="50%" >
+ 
+          <Heading size="4xl" textAlign={"center"} mb={4} fontFamily={"Sigmar"} p={"30"}>Welcome to<br />
+          
+          <Text size="4xl" fontFamily={"Alkatra"} textAlign={"center"} fontSize={"72px"} color={"#27E1C1"}>BitBazaar</Text> </Heading>
+        
+      <Text fontSize="2xl" mb={8} textAlign={"center"} fontFamily={"Montserrat"}>Your one-stop destination for cryptocurrency.</Text>
+      <Button variant="solid" alignContent={"center"} colorScheme="#FED049" size="lg" p={"8"}>Get started</Button>
+      <Button variant="ghost" alignContent={"center"} colorScheme="#FED049" size="lg" p={"8"}>Contact Us</Button>
     </Box>
-    
+    <Box w="50%" position="relative">
+    <motion.div
+      style={{
+        height: "20vh",
+      }}
+        animate={{
+          translateY: "20px",
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+      >
+      <Image
+        src={Hero}
+        alt="Animated Picture"
+        right="-10%"
+      />
+      </motion.div>
+    </Box>
+  </Flex>
   )
 }
 
