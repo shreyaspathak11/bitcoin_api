88b0d00e656a4e9973c6fe56ff95de4fc0ab9da1
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-05-02 00:34:19 +0530

    hero section complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,104 @@
 
-import React from 'react'
-import { Box, Flex, Heading, Text, Button, Image } from "@chakra-ui/react";
+import React from 'react';
+import { Box, Flex, Heading, Text, Button, Image,} from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import Hero from '../assets/increase.png'
 
 const Home = () => {
-
-
   return (
-    <Flex p={4} align="center" justify="center">
-    <Box w="50%" >
- 
-          <Heading size="4xl" textAlign={"center"} mb={4} fontFamily={"Sigmar"} p={"30"}>Welcome to<br />
-          
-          <Text size="4xl" fontFamily={"Alkatra"} textAlign={"center"} fontSize={"72px"} color={"#27E1C1"}>BitBazaar</Text> </Heading>
-        
-      <Text fontSize="2xl" mb={8} textAlign={"center"} fontFamily={"Montserrat"}>Your one-stop destination for cryptocurrency.</Text>
-      <Button variant="solid" alignContent={"center"} colorScheme="#FED049" size="lg" p={"8"}>Get started</Button>
-      <Button variant="ghost" alignContent={"center"} colorScheme="#FED049" size="lg" p={"8"}>Contact Us</Button>
-    </Box>
-    <Box w="50%" position="relative">
-    <motion.div
-      style={{
-        height: "20vh",
-      }}
-        animate={{
+    <>
+    <Flex
+      height="100vh"
+      justify="center"
+      alignItems="center"
+      bgGradient="linear(to-b, #1A202C, #2D3748)"
+    >
+      <Box
+        w="60%"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        color="white"
+      >
+        <Heading
+          size="4xl"
+          textAlign="center"
+          fontFamily="Alkatra"
+          mb={4}
+          letterSpacing="wide"
+        >
+          Welcome to<br />
+          <Text
+            size="4xl"
+            fontFamily="Alkatra"
+            color="#FCE22A"
+            fontWeight="bold"
+          >
+            BitBazaar
+          </Text>
+        </Heading>
+        <Text
+          fontSize="2xl"
+          mb={8}
+          textAlign="center"
+          fontFamily="Alkatra"
+          letterSpacing="wide"
+        >
+          Your one-stop destination for cryptocurrency.
+        </Text>
+        <Flex
+          flexDirection={{ base: "column", md: "row" }}
+          justifyContent="center"
+          alignItems="center"
+          gap={4}
+        >
+          <Button
+            variant="solid"
+            colorScheme="yellow"
+            size="lg"
+            p={{ base: 6, md: 8 }}
+            fontSize={{ base: "lg", md: "xl" }}
+            letterSpacing="wide"
+            fontWeight="bold"
+            _hover={{ bg: "#FCE22A", color: "#1A202C" }}
+          >
+            Get started
+          </Button>
+          <Button
+            variant="outline"
+            colorScheme="yellow"
+            size="lg"
+            p={{ base: 6, md: 8 }}
+            fontSize={{ base: "lg", md: "xl" }}
+            letterSpacing="wide"
+            fontWeight="bold"
+            _hover={{ bg: "#FCE22A", color: "#1A202C" }}
+          >
+            Contact us
+          </Button>
+        </Flex>
+      </Box>
+      <Box w="50%">
+        <motion.div
+          animate={{
           translateY: "20px",
         }}
         transition={{
-          duration: 2,
+          duration: 0.5,
           repeat: Infinity,
           repeatType: "reverse",
         }}
-      >
-      <Image
-        src={Hero}
-        alt="Animated Picture"
-        right="-10%"
-      />
-      </motion.div>
-    </Box>
-  </Flex>
+        >   
+        <Image src={Hero} alt="Animated Picture" justifyContent={"center"} />
+        </motion.div>
+      
+
+          
+        
+      </Box>
+    </Flex>
+    </>
   )
 }
 
