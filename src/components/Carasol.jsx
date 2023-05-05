fd77e4e9f9f06d40b59f3984a0a920b5404b9ddc
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-05-06 01:23:41 +0530

    some changes

--- src/components/Carasol.jsx
+++ src/components/Carasol.jsx
@@ -27,10 +27,10 @@ function Carousel() {
             backgroundPosition="center"
           >
             <Box textAlign="center" >
-              <Heading as="h2" size="4xl"  mb={4} color={'blackAlpha.800'} fontFamily={"Alkatra"} _hover={{ color: "Yellow", transform: "scale(1.05)"}}>
+              <Heading as="h2" size="4xl"  mb={4} color={'White'} fontFamily={"Alkatra"} _hover={{ color: "Yellow", transform: "scale(1.05)"}}>
                 Welcome to BitBazaar
               </Heading>
-              <Text fontSize="3xl" fontWeight="semibold" color={'blackAlpha.800'} fontFamily={"Alkatra"} _hover={{ color: "Yellow", transform: "scale(1.05)"}}>
+              <Text fontSize="3xl" fontWeight="semibold" color={'White'} fontFamily={"Alkatra"} _hover={{ color: "Yellow", transform: "scale(1.05)"}}>
                 Your one-stop shop for all things Bitcoin.
               </Text>
             </Box>
@@ -47,11 +47,10 @@ function Carousel() {
             backgroundPosition="center"
           >
             <Box textAlign="top">
-              <Heading as="h2" size="4xl" mb={3} color={"#FCE22A"} fontFamily={"Alkatra"} _hover={{ bg: "white", color: "teal.500", transform: "scale(1.05)",boxShadow: "xl"}}>
-                Invest in Bitcoin
-              </Heading>
-              <Text fontSize="3xl" fontWeight="semibold" color={"#FCE22A"} fontFamily={"Alkatra"} _hover={{ bg: "white", color: "teal.500", transform: "scale(1.05)",boxShadow: "xl"}}>
-                Discover the benefits of investing in Bitcoin.
+              <Heading as="h2" size="4xl" mb={3} color={"#FCE22A"} fontFamily={"Alkatra"} _hover={{  color: "teal.500", transform: "scale(1.05)",boxShadow: "xl"}}>
+                Invest in Cryptocurrencies</Heading>
+              <Text fontSize="3xl" fontWeight="semibold" color={"#FCE22A"} fontFamily={"Alkatra"} _hover={{ color: "teal.500", transform: "scale(1.05)",boxShadow: "xl"}}>
+                Discover the benefits of investing in Future.
               </Text>
             </Box>
           </Flex>
